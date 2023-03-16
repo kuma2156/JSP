632dad2c920b26ab6fe6c30d01bdb899724cba9a
ddingyu <116462554+kuma2156@users.noreply.github.com>
2023-03-16 16:07:16 +0900

    Add files via upload

---
+++ chapter06/src/sec03/ex01/LoginSevlet3.java
@@ -0,0 +1,67 @@
+package sec03.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.Servlet;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginSevlet2
+ */
+@WebServlet("/Login3")
+public class LoginSevlet3 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		System.out.println("init 메소드 호출");
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+		System.out.println("destroy 메소드 호출");
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("doget 메소드 호출");
+		
+		// 폰트설정
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		
+		PrintWriter out = response.getWriter();
+		
+		// id pw 가져오기
+		String user_id = request.getParameter("user_id");
+		String user_pw = request.getParameter("user_pw");
+		
+		String data = "<html>";
+		data += "<body>";
+		
+		data += "아이디 : " + user_id;
+		data += "<br>";
+		data += "비밀번호 : " + user_pw;
+		
+		data += "</body>";
+		data += "</html>";
+		
+		out.print(data);
+	}
+
+}
