fa69073c07a31139915e9b0f9eeeff32d07cc3c6
ddingyu <116462554+kuma2156@users.noreply.github.com>
2023-04-11 13:12:49 +0900

    Add files via upload

---
+++ assign1/src/ex6_8/loginservlet.java
@@ -0,0 +1,72 @@
+package ex6_8;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+
+public class loginservlet extends HttpServlet
+{
+	private static final long serialVersionUID = 1L;
+
+	public void init(ServletConfig config) throws ServletException
+	{
+		System.out.println("init 메소드 호출");
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		System.out.println("doPost 메소드 호출");
+		
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		
+		PrintWriter out = response.getWriter();
+		
+		String id = request.getParameter("user_id");
+		String pw = request.getParameter("user_pw");
+		
+		System.out.println("아이디 : " + id);
+		System.out.println("비밀번호 : " + pw);
+		
+		// 아이디 값이 비워있지 않고 아이디의 길이가 0이 아닐 경우 실행
+		if(id != null && (id.length() != 0))
+		{
+			out.print("<html>");
+			out.print("<body>");
+			
+			out.print(id + "님이 로그인 하셨습니다.");
+			out.print("<br>");
+			
+			out.print("</body>");
+			out.print("</html>");
+		}
+		else
+		{
+			out.print("<html>");
+			out.print("<body>");
+			
+			out.print("아이디를 입력하셔야합니다.");
+			out.print("<br>");
+			out.print("<a href='http://localhost:8090/assign1/login.html'> 로그인 창으로 이동 </a>");
+			
+			out.print("</body>");
+			out.print("</html>");
+		}
+	} 
+
+	public void destroy() 
+	{
+		System.out.println("destroy 메소드 호출");
+	}
+
+
+
+}
